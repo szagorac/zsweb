3ddfa056beaf97877fc6d7250307b4c4f64ad910
szagorac <slavko@zagorac.com>
2022-05-27 20:22:08 +0100

    dialogs noise filter

--- src/js/adncDialogs.js
+++ src/js/adncDialogs.js
@@ -531,7 +531,7 @@ var zscore = (function (u, n, s, a, m, syn, win, doc) {
                 votes = 1;
             }
             a.setPlayerVolume(0.0, config.audioFadeInMs);
-            var vol = u.mapRange(Math.abs(value), 0.0, Math.abs(maxVal), 0.0, config.maxNoiseLevel);  
+            var vol = u.mapRange(Math.abs(value), 1.0, Math.abs(maxVal), 0.2, config.maxNoiseLevel);  
         
             var minIdx = 0;
             var maxIdx = state.noiseFreqs.length -1;
